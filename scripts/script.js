b83e880861295f8f3e4c47c57d4891ddca76f174
StinkyJeans <131233083+StinkyJeans@users.noreply.github.com>
2024-02-29 19:57:02 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,42 @@
-function generateQuestion() {
-    const num1 = Math.floor(Math.random() * 1000);
-    const num2 = Math.floor(Math.random() * 1000);
-    const operators = ["+", "-", "*", "/", "%"];
-    const randomOperator = operators[Math.floor(Math.random() * operators.length)];
-    let answer;
+window.onload = function() {
+    var studentName = prompt("Please enter your name : ");
+    
+    function generateQuestion() {
+        var num1 = Math.floor(Math.random() * 1000);
+        var num2 = Math.floor(Math.random() * 1000);
+        var operators = ["+", "-", "*"];
+        var randOp = operators[Math.floor(Math.random() * operators.length)];
+        let answer;
 
-    switch(randomOperator) {
-        case "+":
-            answer = num1 + num2;
-            break;
-        case "-":
-            answer = num1 - num2;
-            break;
-        case "*":
-            answer = num1 * num2;
-            break;
-        case "/":
-            answer = num1 / num2;
-            break;
-        case "%":
-            answer = num1 % num2;
-            break;
-        default:
-            break;
+        switch(randOp) {
+            case "+":
+                answer = num1 + num2;
+                break;
+            case "-":
+                answer = num1 - num2;
+                break;
+            case "*":
+                answer = num1 * num2;
+                break;
+        }
+
+        return { question: `What is ${num1} ${randOp} ${num2}?`, answer: answer };
+    }
+    
+    function displayQuestions() {
+        let score = 0;
+
+        for (let i = 0; i < 10; i++) {
+            var currentQuestion = generateQuestion();
+            var userAnswer = parseInt(prompt(`${studentName}, ${currentQuestion.question}`), 10);
+            
+            if (!isNaN(userAnswer) && userAnswer === currentQuestion.answer) {
+                score++;
+            }
+        }
+        
+        alert(`${studentName}, your total score is: ${score}`);
     }
 
-    return { question: `What is ${num1} ${randomOperator} ${num2}?`, answer: answer };
+    displayQuestions();
 }
-
-let currentQuestion = generateQuestion();
-let score = 0;
-
-function displayQuestion() {
-    document.getElementById('question').innerText = currentQuestion.question;
-}
-
-function checkAnswer() {
-    const userAnswer = parseInt(document.getElementById('answer').value.trim(), 10);
-    const correctAnswer = currentQuestion.answer;
-
-    if (userAnswer === correctAnswer) {
-        alert('You are correct!');
-        score++; 
-    } else {
-        alert(`You are wrong! The correct answer is ${correctAnswer}.`);
-    }
-}
-
-function generateNewQuestion() {
-    currentQuestion = generateQuestion();
-    displayQuestion();
-    document.getElementById('answer').value = ""; 
-}
-
-displayQuestion();
